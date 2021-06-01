dd1b7039b07006bd4da355c32ac6f7f7d2d58c7f
Reinaldo Avelino <73717173+ReinaldoAvelino@users.noreply.github.com>
2021-05-31 21:24:39 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,37 +19,46 @@ void listarPerfis(RedeSocial* R) {
     }
 }
 
-int main() {
+void interface() {
 
     RedeSocial* PoliBook = new RedeSocial();
 
-    
+    string opcao_string = "";
     int opcao = -1;
-    while (opcao != 0) {
+    while (opcao_string != "0") {
         cout << "Escolha uma opcao"   << endl
              << "1) Cadastrar Pessoa" << endl
              << "2) Cadastrar Pagina" << endl
              << "3) Logar"            << endl
              << "0) Terminar"         << endl;
-        cin >> opcao;
+
+        while (true) {
+            getline(cin, opcao_string);
+
+            // Convert from string to number safely
+            stringstream myStream(opcao_string);
+            if (myStream >> opcao) break;
+            cout << "Numero invalido. Tente novamente." << endl;
+        }
 
         switch (opcao) {
             case 1:
             {
-                string nome = "nada", verificada = "nada";
+                string nome;
+                string verificada = "";
                 cout << "Informe os dados da pessoa" << endl;
                 cout << "Nome: ";
-                cin >> nome;
+                getline(cin, nome);
 
                 while (verificada != "s" && verificada != "n") {
                     cout << "Verificada (s/n)? ";
-                    cin >> verificada;
+                    getline(cin, verificada);
 
                     if(verificada == "s") {
 
                         string email;
                         cout << "Email: ";
-                        cin >> email;
+                        getline(cin, email);
 
                         PoliBook->adicionar(new PessoaVerificada(nome, email));
 
@@ -60,7 +69,7 @@ int main() {
                     }
                 }
                 cout << endl;
-            }    
+            }
             break;
 
             case 2:
@@ -68,14 +77,24 @@ int main() {
                 string nome;
                 cout << "Informe os dados da pagina" << endl;
                 cout << "Nome: ";
-                cin >> nome;
+                getline(cin, nome);
                 cout << "Proprietario:" << endl;
                 listarPerfis(PoliBook);
-                int numero;
-                cout << "Digite o numero ou 0 para cancelar: ";
-                cin >> numero;
+
+                string numero_string = "";
+                int numero = -1;
+
+                while (true) {
+                    cout << "Digite o numero ou 0 para cancelar: ";
+                    getline(cin, numero_string);
+
+                    // Convert from string to number safely
+                    stringstream myStream(numero_string);
+                    if (myStream >> numero) break;
+                }
+
                 if (numero != 0) {
-                    if ( PerfilVerificado* pv = dynamic_cast<PerfilVerificado*>(PoliBook->getPerfis()[numero-1]) ) {
+                    if ( PessoaVerificada* pv = dynamic_cast<PessoaVerificada*>(PoliBook->getPerfis()[numero-1]) ) {
                         PoliBook->adicionar( new Pagina(nome, pv) );
                     }
                     else {
@@ -90,33 +109,162 @@ int main() {
             {
                 cout << "Escolha um perfil:" << endl;
                 listarPerfis(PoliBook);
-                int numero;
-                cout << "Digite o numero ou 0 para cancelar: ";
-                cin >> numero;
+
+                string numero_string = "";
+                int numero = -1;
+                while (true) {
+                    cout << "Digite o numero ou 0 para cancelar: ";
+                    getline(cin, numero_string);
+
+                    // Convert from string to number safely
+                    stringstream myStream(numero_string);
+                    if (myStream >> numero) break;
+                }
+
                 if (numero != 0) {
                     Perfil* p = PoliBook->getPerfis()[numero-1];
                     cout << p->getNome();
-                    if ( PerfilVerificado* pv = dynamic_cast<PerfilVerificado*>(p) ) {
+                    if ( PessoaVerificada* pv = dynamic_cast<PessoaVerificada*>(p) ) {
                         cout << " - " << pv->getEmail();
                     }
                     cout << endl;
                     if ( Pagina* pag = dynamic_cast<Pagina*>(p) ) {
-                        cout << "Proprietario " << pag->getProprietario()->getNome(); << endl;
+                        cout << "Proprietario " << pag->getProprietario()->getNome() << endl;
                     }
                     cout << "Contatos: " << p->getQuantidadeDeContatos() << endl;
-                    cout << "- - -" << endl;             
-                    cout << "Escolha uma opcao:" << endl;
-                    
-                    
+                    cout << "---" << endl;
+
+                    int acao = -1;
+                    string acao_string = "";
+                    while (acao != 0) {
+                        cout << "Escolha uma opcao:" << endl
+                             << "1) Fazer Postagem" << endl
+                             << "2) Ver postagens dos contatos" << endl
+                             << "3) Adicionar contato" << endl
+                             << "0) Deslogar" << endl;
+                        while (true) {
+
+                            getline(cin, acao_string);
+
+                            // Convert from string to number safely
+                            stringstream myStream(acao_string);
+                            if (myStream >> acao) break;
+                            cout << "Tente novamente" << endl;
+                        }
+
+                        switch (acao) {
+                            case 1:
+                            {
+                                cout << "Story (s/n): ";
+                                string resposta;
+                                getline(cin, resposta);
+
+                                int data = -1;
+                                string data_string = "";
+                                while (true) {
+                                    cout << "Data: ";
+                                    getline(cin, data_string);
+
+                                    // Convert from string to number safely
+                                    stringstream myStream(data_string);
+                                    if (myStream >> data) break;
+                                }
+
+                                int dataDeFim = -1;
+                                string dataDeFim_string = "";
+                                if (resposta == "s") {
+                                    while (true) {
+                                        cout << "Data de fim: ";
+                                        getline(cin, dataDeFim_string);
+
+                                        // Convert from string to number safely
+                                        stringstream myStream(dataDeFim_string);
+                                        if (myStream >> dataDeFim) break;
+                                    }
+                                }
+
+                                string texto;
+                                cout << "Texto: ";
+                                getline(cin, texto);
+                                if (resposta == "s") {
+                                    p->adicionarPostagem(new Story(texto, data, dataDeFim, p));
+                                }
+                                else {
+                                    p->adicionarPostagem(new Postagem(texto, data, p));
+                                }
+                                cout << endl;
+                            }
+                            break;
+
+                            case 2:
+                            {
+                                int data = -1;
+                                int quantidade;
+                                string data_string = "";
+                                while (true) {
+                                    cout << "Data: ";
+                                    getline(cin, data_string);
+
+                                    // Convert from string to number safely
+                                    stringstream myStream(data_string);
+                                    if (myStream >> data) break;
+                                }
+
+                                cout << "Postagens dos ultimos 3 dias:" << endl;
+                                Postagem** postagensDosContatos = p->getPostagensDosContatos(data, quantidade);
+                                for (int i = 0; i < quantidade; i++) {
+                                    postagensDosContatos[i]->imprimir();
+                                }
+                                cout << endl;
+                            }
+                            break;
+
+                            case 3:
+                            {
+                                cout << "Perfil:" << endl;
+                                listarPerfis(PoliBook);
+
+                                int escolha = -1;
+                                string escolha_string = "";
+                                while (true) {
+                                    cout << "Digite o numero ou 0 para cancelar: ";
+                                    getline(cin, escolha_string);
+
+                                    // Convert from string to number safely
+                                    stringstream myStream(escolha_string);
+                                    if (myStream >> escolha) break;
+                                }
+
+                                if (escolha != 0) {
+                                    if( p->adicionarContato(PoliBook->getPerfis()[escolha-1]) ) {
+                                        cout << "Adicionado com sucesso" << endl;
+                                    }
+                                    else {
+                                        cout << "Contato nao adicionado" << endl;
+                                    }
+                                }
+                                else cout << "Contato nao adicionado" << endl;
+                                cout << endl;
+                            }
+                            break;
+                        }
+                    }
                 }
                 cout << endl;
             }
             break;
 
             case 0:
+            {
                 delete PoliBook;
+            }
             break;
         }
     }
+}
+
+int main() {
+    interface();
     return 0;
 }
+
