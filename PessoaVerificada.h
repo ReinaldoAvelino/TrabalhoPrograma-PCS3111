553e08da431b333b57deb343e2932854c9019ab4
Reinaldo Avelino <73717173+ReinaldoAvelino@users.noreply.github.com>
2021-07-05 00:13:39 -0300

    Update PessoaVerificada.h

--- PessoaVerificada.h
+++ PessoaVerificada.h
@@ -3,11 +3,15 @@
 
 #include "Pessoa.h"
 
+#include <string>
+
 class PessoaVerificada: public Pessoa {
     public:
         PessoaVerificada(string nome, string email);
         PessoaVerificada(string nome);
+        PessoaVerificada(string nome, string email, int id);
         virtual ~PessoaVerificada();
+    
         string getEmail();
         void imprimir();
     private:
