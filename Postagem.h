71760a2c5f126ae99f4b32a2ce0859fc014d60f8
Reinaldo Avelino <73717173+ReinaldoAvelino@users.noreply.github.com>
2021-07-05 00:25:15 -0300

    Update Postagem.h

--- Postagem.h
+++ Postagem.h
@@ -2,23 +2,27 @@
 #define POSTAGEM_H
 
 #include "Perfil.h"
+
 #include <string>
+
 using namespace std;
 
 class Perfil;
 
 class Postagem {
+public:
+    Postagem(string texto, int data, Perfil* autor);
+    virtual ~Postagem();
+    
+    string getTexto();
+    int getData();
+    Perfil* getAutor();
+    
+    virtual void imprimir();
 protected:
     string texto;
     int data;
     Perfil* autor;
-public:
-    string getTexto();
-    int getData();
-    Perfil* getAutor();
-    Postagem(string texto, int data, Perfil* autor);
-    virtual ~Postagem();
-    virtual void imprimir();
 };
 
 #endif // POSTAGEM_H
